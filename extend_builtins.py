90f1e32bfbc9de7f4fac7a13778b5171831d74ea
Lapis <lapis0875@kakao.com>
2021-01-22 00:15:45 +0900

    feat: Update extend_builtins.py
    Working on PyOptional<T>, for experiment.

--- extend_builtins.py
+++ extend_builtins.py
@@ -1,13 +1,14 @@
-from typing import Iterable, Callable, Any, List
+from typing import Iterable, Callable, Any, List, TypeVar, Generic, Optional
 
 Function = Callable[..., Any]       # (item) -> result
 Consumer = Callable[..., None]      # (item) -> {} (Return nothing)
 
+T = TypeVar('T')
 
 def extend_filter():
     """Extend filter object for utility."""
     if not getattr(filter, 'find_first', False):
-        filter.find_first = lambda self: next(self)
+        filter.find_first = lambda self: next(self, default=None)
 
 # class map(object):
 #     def __init__(func: Function, *iterables: Iterable):   ...
@@ -22,6 +23,17 @@ class compute(map):
     def run(self):
         """Run function"""
         tuple(self)     # Loop all iterables in map(parent class).
+
+
+class PyOptional(Generic[T]):
+    def __init__(self, item: Optional[T]) -> None:
+        self._item = item
+
+    @property
+    def isNone(self) -> bool:
+        return self._item is None
+
+    # TODO : Java의 Optional<T>를 구현. (Nullable한 값에 대한 functional한 처리)
 
 
 def init():
